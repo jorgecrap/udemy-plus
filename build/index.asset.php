dbacaa6e39a7282be12e44f783505bdabc18583e
jorgecrap <jorgecrap@gmail.com>
2023-11-16 21:45:54 +0100

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1755ec7ea21d0d5ce20a');
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Udemy
+ * Author URI:        https://udemy.com
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+if(!function_exists('add_action')){ 
+    echo 'Parece que has acabado aquí por accidente :)';
+    exit;
+}
